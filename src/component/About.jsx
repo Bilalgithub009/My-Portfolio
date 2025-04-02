4d1f0306c808fbcf948626f26abbe71cc0dfd105
Bilalgithub009 <m.bilaljaliyawala@gmail.com>
2025-04-02 20:13:18 +0500

    Update About.jsx
    image

--- src/component/About.jsx
+++ src/component/About.jsx
@@ -19,7 +19,8 @@ function About() {
             <img
               alt="profile"
               className="imagecards hover:scale-110 sm:max-w-full sm:max-h-full  transition-transform duration-500 lg:w-1/2 sm:w-3/4 w-full h-auto object-cover object-center rounded shadow-lg"
-              src={"../src/images/bilal.jpg"}
+              // src={"../src/images/bilal.jpg"}
+              src={require("../images/bilal.jpg")}
               
             />
 
